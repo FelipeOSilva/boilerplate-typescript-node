9da269a59d210a5cb383333593401cf7b62e6a46
Felipe Silva <felipe_96sp@msn.com>
2021-06-21 11:28:16 -0300

    Fix import

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express, { response } from "express"
+import express from 'express'
 
 const app = express()
 
 app.get('/', (request, response) =>
-    response.json({ message: "Hello World" })
+  response.json({ message: 'Hello World' })
 )
 
 app.listen(3333)
